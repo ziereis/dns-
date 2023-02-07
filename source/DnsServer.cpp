fdadc365928225ffeb545516de70b2d92779b00e
Thomas <thomasziereis@web.de>
2023-02-07 20:30:25 +0100

    add basic lookup but also race condition i think

--- source/DnsServer.cpp
+++ source/DnsServer.cpp
@@ -5,25 +5,41 @@
 #include "DnsServer.h"
 namespace Dns
 {
-    DnsServer::DnsServer(int thread_count)
-            : thread_count{thread_count}
-            , socket(io)
+    DnsServer::DnsServer()
+            : client_socket(io)
+            , lookup_socket(io)
     {}
 
     void DnsServer::start_server(uint16_t port)
     {
         boost::system::error_code ec;
 
-        ip::udp::endpoint ep(ip::address_v4::any(), port);
-        socket.open(ip::udp::v4(), ec);
-        socket.bind(ep, ec);
+        ip::udp::endpoint client_ep(ip::address_v4::any(), port);
+        client_socket.open(ip::udp::v4(), ec);
+        client_socket.bind(client_ep, ec);
+
+        ip::udp::endpoint server_ep(ip::address_v4::any(), 52346);
+        lookup_socket.open(ip::udp::v4(), ec);
+        lookup_socket.bind(client_ep, ec);
 
         std::array<uint8_t, DNS_BUF_SIZE> buf{};
-        socket.async_receive(buffer(buf), [&buf, this] (const auto& ec, std::size_t bytes_read){
+        client_socket.async_receive_from(buffer(buf), client, [&buf, this] (const auto& ec, std::size_t bytes_read){
             handle_incoming_request(ec, bytes_read, buf);
         });
 
         io.run();
+    }
+
+
+
+    void DnsServer::lookup(std::array<uint8_t, DNS_BUF_SIZE> buf)
+    {
+        ip::udp::endpoint name_server(ip::address::from_string("8.8.8.8"), 53);
+
+        lookup_socket.async_send_to(buffer(buf), name_server, [this](const auto&ec, size_t bytes_sent) {
+            handle_lookup(ec, bytes_sent);});
+
+
     }
 
     void DnsServer::handle_incoming_request(const boost::system::error_code& ec,
@@ -32,12 +48,28 @@ namespace Dns
     {
         if(ec) return;
 
-        Dns::DnsPacket packet(buf, bytes_read);
+        lookup(buf);
 
-        socket.async_receive(buffer(buf), [&buf, this](const auto& ec, std::size_t bytes_read){
+        client_socket.async_receive_from(buffer(buf), client, [&buf, this](const auto& ec, std::size_t bytes_read){
             handle_incoming_request(ec, bytes_read, buf);
         });
 
+    }
+
+    void DnsServer::handle_lookup(const boost::system::error_code &ec, size_t bytes_read) {
+
+        std::array<uint8_t, DNS_BUF_SIZE> buf{};
+        lookup_socket.async_receive(buffer(buf), [&buf, this] (const auto& ec, std::size_t bytes_read){
+            handle_server_response(ec, bytes_read, buf);
+        });
+
+    }
+
+    void DnsServer::handle_server_response(const boost::system::error_code &ec, size_t bytes_read,
+                                           std::array<uint8_t, DNS_BUF_SIZE> buf) {
+        client_socket.async_send_to(buffer(buf), client, [](const auto& ec, std::size_t bytes_read) {
+            std::cout << "return packet to client" << std::endl;
+        });
     }
 
 }
--- source/DnsServer.h
+++ source/DnsServer.h
@@ -7,18 +7,27 @@ namespace Dns
     class DnsServer
     {
     public:
-        explicit DnsServer(int thread_count =1);
+        explicit DnsServer();
 
         void start_server(uint16_t port);
 
         void handle_incoming_request(const boost::system::error_code& ec,
                                      size_t bytes_read,
                                      std::array<uint8_t, DNS_BUF_SIZE> buf);
+
+        void lookup(std::array<uint8_t, DNS_BUF_SIZE> buf);
+
+        void handle_lookup(const boost::system::error_code& ec,
+                           size_t bytes_read);
+
+        void handle_server_response(const boost::system::error_code& ec,
+                                    size_t bytes_read,
+                                    std::array<uint8_t, DNS_BUF_SIZE> buf);
     private:
-        int thread_count;
-        std::vector<std::thread> thread_pool;
         io_context io;
-        ip::udp::socket socket;
+        ip::udp::socket client_socket;
+        ip::udp::socket lookup_socket;
+        ip::udp::endpoint client;
 
     };
 }
